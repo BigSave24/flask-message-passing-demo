10e310329c5daa3bce53d267c6f4a2f403761d19
kedmond24 <kedmond24@live.com>
2022-11-09 00:16:51 -0500

    UPDATE correct import format

--- enum.py
+++ enum.py
@@ -2,9 +2,6 @@ from enum import Enum
 from multiprocessing.dummy import Process
 from queue import Queue
 
-
-from enum import Enum
-
 class Status(Enum):
     Queued = 'Queued'
     Processing = 'Processing'
